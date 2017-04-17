7d51671e03b3e063c5c9ad2361268a36a06cf302
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2017-04-17 18:37:54 +0200

    ABox

--- logic.go
+++ logic.go
@@ -22,7 +22,12 @@
 
 package goel
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+//// Concepts ////
 
 // ELBaseComponents used to save basic information about an EL++ instance.
 // It stores only the number of each base concept, nothing else.
@@ -87,12 +92,21 @@ func (name NamedConcept) String() string {
 	return fmt.Sprintf("A(%d)", name)
 }
 
-// NominalConcept is a nominal concept of the form {a}, identified by id.
+// Nominal is a nominal a ∈ N_I, identified by id.
 // Each nominal a ∈ N_I is encoded as a unique integer with this type.
-type NominalConcept uint
+type Nominal uint
+
+func (nominal Nominal) String() string {
+	return fmt.Sprintf("a(%d)", nominal)
+}
+
+// NominalConcept is a nominal concept of the form {a}, identified by id.
+// A Nominal is encoded by the type Nominal, a NominalConcept is just the usage
+// of a Nominal as a Concept.
+type NominalConcept Nominal
 
 func (nominal NominalConcept) String() string {
-	return fmt.Sprintf("a(%d)", nominal)
+	return fmt.Sprintf("{a(%d)}", nominal)
 }
 
 // Role is an EL++ role r ∈ N_R, identifiey by id.
@@ -142,4 +156,97 @@ func NewExistentialConcept(r Role, c Concept) *ExistentialConcept {
 
 func (existential *ExistentialConcept) String() string {
 	return fmt.Sprintf("∃ %v.%v", existential.R, existential.C)
+}
+
+//// TBox ////
+
+// GCIConstraint is a general concept inclusion of the form C ⊑ D.
+type GCIConstraint struct {
+	C, D Concept
+}
+
+// NewGCIConstraint returns a new general concept inclusion C ⊑ D.
+func NewGCIConstraint(c, d Concept) *GCIConstraint {
+	return &GCIConstraint{C: c, D: d}
+}
+
+func (gci *GCIConstraint) String() string {
+	return fmt.Sprintf("%v ⊑ %v", gci.C, gci.D)
+}
+
+// RoleInclusion is a role inclusion of the form r1 o ... o rk ⊑ r.
+type RoleInclusion struct {
+	// LHS contains the left-hand side r1 o ... o rk.
+	LHS []Role
+	// RHS is the right-hand side r.
+	RHS Role
+}
+
+// NewRoleInclusion returns a new role inclusion r1 o ... o rk ⊑ r.
+func NewRoleInclusion(lhs []Role, rhs Role) *RoleInclusion {
+	return &RoleInclusion{LHS: lhs, RHS: rhs}
+}
+
+func (ri *RoleInclusion) String() string {
+	// To use the strings.Join function we first generate a slice of the roles
+	// as string
+	strs := make([]string, len(ri.LHS))
+	for i, r := range ri.LHS {
+		strs[i] = r.String()
+	}
+	return fmt.Sprintf("%s ⊑ %s", strings.Join(strs, "o"), ri.RHS.String())
+}
+
+// TBox describes a TBox as a set of GCIs and RIs.
+type TBox struct {
+	GCIs []*GCIConstraint
+	RIs  []*RoleInclusion
+}
+
+// NewTBox returns a new TBox.
+func NewTBox(gcis []*GCIConstraint, ris []*RoleInclusion) *TBox {
+	return &TBox{GCIs: gcis, RIs: ris}
+}
+
+//// ABox ////
+
+// ConceptAssertion is an concept assertion of the form C(a).
+type ConceptAssertion struct {
+	C Concept
+	A Nominal
+}
+
+// NewConceptAssertion returns a new concept assertion C(a).
+func NewConceptAssertion(c Concept, a Nominal) *ConceptAssertion {
+	return &ConceptAssertion{C: c, A: a}
+}
+
+func (ca *ConceptAssertion) String() string {
+	return fmt.Sprintf("%v(%v)", ca.C, ca.A)
+}
+
+// RoleAssertion is a role assertion of the form r(a, b).
+type RoleAssertion struct {
+	R    Role
+	A, B Nominal
+}
+
+// NewRoleAssertion returns a new role assertion r(a, b).
+func NewRoleAssertion(r Role, a, b Nominal) *RoleAssertion {
+	return &RoleAssertion{R: r, A: a, B: b}
+}
+
+func (ra *RoleAssertion) String() string {
+	return fmt.Sprintf("%v(%v, %v)", ra.R, ra.A, ra.B)
+}
+
+// ABox describes an ABox as a set of concept assertions and role assertions.
+type ABox struct {
+	ConceptAssertions []*ConceptAssertion
+	RoleAssertions    []*RoleAssertion
+}
+
+// NewABox returns a new ABox.
+func NewABox(conceptAssertions []*ConceptAssertion, roleAssertions []*RoleAssertion) *ABox {
+	return &ABox{ConceptAssertions: conceptAssertions, RoleAssertions: roleAssertions}
 }
