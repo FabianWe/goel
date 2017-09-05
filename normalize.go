2c5d11ccf609b2434bc57eb6cc107beda57c4e93
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2017-09-05 04:13:10 +0200

    normalization phase two

--- normalize.go
+++ normalize.go
@@ -23,6 +23,7 @@
 package goel
 
 import (
+	"fmt"
 	"log"
 	"reflect"
 	"sync"
@@ -46,13 +47,78 @@ func (dist *IntDistributor) Next() uint {
 }
 
 type phaseOneResult struct {
+	distributor     *IntDistributor
 	waiting         []*GCIConstraint
 	intermediateRes []*GCIConstraint
-	distributor     *IntDistributor
 	// for CIs already in normal form we can just add them already for phase 2
 	intermediateCIs     []*NormalizedCI
 	intermediateCILeft  []*NormalizedCILeftEx
 	intermediateCIRight []*NormalizedCIRightEx
+}
+
+func newPhaseOneResult(distributor *IntDistributor) *phaseOneResult {
+	return &phaseOneResult{distributor: distributor}
+}
+
+func (res *phaseOneResult) union(other *phaseOneResult) {
+	var wg sync.WaitGroup
+	wg.Add(4)
+
+	go func() {
+		defer wg.Done()
+		res.intermediateRes = append(res.intermediateRes, other.intermediateRes...)
+	}()
+
+	go func() {
+		defer wg.Done()
+		res.intermediateCIs = append(res.intermediateCIs, other.intermediateCIs...)
+	}()
+
+	go func() {
+		defer wg.Done()
+		res.intermediateCILeft = append(res.intermediateCILeft, other.intermediateCILeft...)
+	}()
+
+	go func() {
+		defer wg.Done()
+		res.intermediateCIRight = append(res.intermediateCIRight, other.intermediateCIRight...)
+	}()
+
+	wg.Wait()
+}
+
+type phaseTwoResult struct {
+	distributor         *IntDistributor
+	waiting             []*GCIConstraint
+	intermediateCIs     []*NormalizedCI
+	intermediateCILeft  []*NormalizedCILeftEx
+	intermediateCIRight []*NormalizedCIRightEx
+}
+
+func newPhaseTwoResult(distributor *IntDistributor) *phaseTwoResult {
+	return &phaseTwoResult{distributor: distributor}
+}
+
+func (res *phaseTwoResult) union(other *phaseTwoResult) {
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		res.intermediateCIs = append(res.intermediateCIs, other.intermediateCIs...)
+	}()
+
+	go func() {
+		defer wg.Done()
+		res.intermediateCILeft = append(res.intermediateCILeft, other.intermediateCILeft...)
+	}()
+
+	go func() {
+		defer wg.Done()
+		res.intermediateCIRight = append(res.intermediateCIRight, other.intermediateCIRight...)
+	}()
+
+	wg.Wait()
 }
 
 func NormalizeStepPhaseOne(gci *GCIConstraint, intermediate *phaseOneResult) {
@@ -139,4 +205,99 @@ func NormalizeStepPhaseOne(gci *GCIConstraint, intermediate *phaseOneResult) {
 			// it should never be affected again in phase one?
 		}
 	}
+}
+
+func NormalizeStepPhaseTwo(gci *GCIConstraint, intermediate *phaseTwoResult) {
+	// again a analyze what form we have
+	// check which of the rules of phase two can be applied, if none can be
+	// applied gci must already be in normal form
+	if !gci.C.IsInBCD() && !gci.D.IsInBCD() {
+		// apply NF5
+		newName := NewNamedConcept(intermediate.distributor.Next())
+		first := NewGCIConstraint(gci.C, newName)
+		second := NewGCIConstraint(newName, gci.D)
+		intermediate.waiting = append(intermediate.waiting, first, second)
+		return
+	}
+	switch rhs := gci.D.(type) {
+	case ExistentialConcept:
+		if rhs.C.IsInBCD() {
+			// add result, now lhs must be in bcd, otherwise there was some mistake...
+			// TODO remove this test once everything has been tested 1000 times
+			if !gci.C.IsInBCD() {
+				log.Printf("Found existential restriction on RHS, but the LHS is not in BCD (type %v) in normalization phase 2. MISTAKE\n",
+					reflect.TypeOf(gci.C))
+				return
+			} else {
+				// everything ok, add the result
+				normalized := NewNormalizedCIRightEx(gci.C, rhs.R, rhs.C)
+				intermediate.intermediateCIRight = append(intermediate.intermediateCIRight, normalized)
+			}
+		} else {
+			// apply NF6
+			newName := NewNamedConcept(intermediate.distributor.Next())
+			newExistential := NewExistentialConcept(rhs.R, newName)
+			first := NewGCIConstraint(gci.C, newExistential)
+			second := NewGCIConstraint(newName, rhs.C)
+			intermediate.waiting = append(intermediate.waiting, first, second)
+		}
+	case Conjunction:
+		// aply NF7
+		first := NewGCIConstraint(gci.C, rhs.C)
+		second := NewGCIConstraint(gci.C, rhs.D)
+		intermediate.waiting = append(intermediate.waiting, first, second)
+	default:
+		// none of the rules can be applied in phase two, so it must be in normal
+		// form and we can simply add the result
+		// check for the type of the normal form
+
+		// first case: lhs is in BCD and rhs is in BCD or false
+		if gci.C.IsInBCD() && BCDOrFalse(gci.D) {
+			normalized := NewNormalizedCI(gci.C, nil, gci.D)
+			intermediate.intermediateCIs = append(intermediate.intermediateCIs, normalized)
+			return
+		}
+
+		// second case: lhs is a concjunction, both C1 and C2 (lhs) must be in BCD
+		// and D (rhs) must be in BCD or false
+		if lhs, ok := gci.C.(Conjunction); ok && BCDOrFalse(gci.D) {
+			// now both parts of the conjunction must be in BCD, check for this
+			if lhs.C.IsInBCD() && lhs.D.IsInBCD() {
+				normalized := NewNormalizedCI(lhs.C, lhs.D, rhs)
+				intermediate.intermediateCIs = append(intermediate.intermediateCIs, normalized)
+				return
+			} else {
+				fmt.Printf("Normalization phase two: Found conjunction on LHS, but its parts are not both in the BCD, types: %v and %v\n",
+					reflect.TypeOf(lhs.C), reflect.TypeOf(lhs.D))
+				return
+			}
+		}
+
+		// third case: existential on rhs, this must not be checked because we
+		// checked it before
+
+		// foruth case: existential on lhs, the concept must be in BCD and the
+		// rhs must be in BCD or false
+		if lhs, ok := gci.C.(ExistentialConcept); ok && BCDOrFalse(gci.D) {
+			// now check if condition is satisfied again
+			if lhs.C.IsInBCD() {
+				// TODO bad, don't remeber why...
+				normalized := NewNormalizedCILeftEx(lhs.R, lhs.C, gci.D)
+				intermediate.intermediateCILeft = append(intermediate.intermediateCILeft, normalized)
+				return
+			} else {
+				log.Printf("Normalization phase two: Found existential on LHS, but its concept is not in BCD (type %v): MISTAKE\n",
+					reflect.TypeOf(lhs.C))
+				return
+			}
+		}
+
+		// if we reach this point something went wrong: not in normal form
+		log.Printf("Normalization phase two: Found invalid GCI %v. Types: %v and %v\n",
+			gci, reflect.TypeOf(gci.C), reflect.TypeOf(gci.D))
+	}
+}
+
+type TBoxNormalformBuilder interface {
+	Normalize(tbox *TBox) *NormalizedTBox
 }
